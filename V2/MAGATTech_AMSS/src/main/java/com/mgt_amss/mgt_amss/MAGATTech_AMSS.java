e14b460efa85fb023d0513ea1663e98178f3066e
brahimafa <brahimafassy@gmail.com>
2021-12-26 18:21:34 +0100

    remove missing libs in V2

--- V2/MAGATTech_AMSS/src/main/java/com/mgt_amss/mgt_amss/MAGATTech_AMSS.java
+++ V2/MAGATTech_AMSS/src/main/java/com/mgt_amss/mgt_amss/MAGATTech_AMSS.java
@@ -1,7 +1,5 @@
 package com.mgt_amss.mgt_amss;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.pdf.PdfWriter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
